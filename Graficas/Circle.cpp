eb34e23b136ddd2ed1d15f2948a62777b2644bfa
Alejandra Perez <A01373993@itesm.mx>
2017-08-20 19:53:24 -0500

    Ejercicio Circle

---
+++ Graficas/Circle.cpp
@@ -0,0 +1,32 @@
+#include "Circle.h"
+
+Circle::Circle() {
+	_radius = 1.0;
+	_color = "red";
+}
+
+Circle::Circle(double r){
+	_radius = r;
+	_color = "red";
+}
+
+Circle::Circle(double r, std::string c) {
+	_radius = r;
+	_color = c;
+}
+
+double Circle::GetRadius() {
+	return _radius;
+}
+
+double Circle::GetArea() {
+	return 3.14159 * _radius * _radius;
+}
+
+void Circle::SetRadius(double r) {
+	_radius = r;
+}
+
+void Circle::SetColor(std::string c) {
+	_color = c;
+}
